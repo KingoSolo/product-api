938cf2b70ee036b235e0686f581e889ce8c046c9
Solomon Adeniran <soluwaseun806@gmail.com>
2026-03-15 17:05:50 +0100

    fixed error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserModule } from 'src/users/user.module';
+import { UserModule } from '../users/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtSignOptions } from '@nestjs/jwt';
 
 @Module({
   imports:[UserModule,PassportModule,JwtModule.registerAsync({
     imports:[ConfigModule],
-    useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'), 
-        signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRATION_TIME'), 
-        },
-      }),
+       useFactory: (configService: ConfigService) => {
+        const expiresIn = configService.getOrThrow<string>('JWT_EXPIRATION_TIME');
+
+        return {
+          secret: configService.getOrThrow<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: expiresIn as JwtSignOptions['expiresIn'],
+          },
       inject: [ConfigService],
-  })],
+    }}})],
   controllers: [AuthController],
   providers: [AuthService]
 })
