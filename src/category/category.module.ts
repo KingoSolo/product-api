a7c0d61d1c717bad309e350a5e4fec8acbe60fae
Solomon Adeniran <soluwaseun806@gmail.com>
2026-04-22 23:01:23 +0100

    updated exports

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { CategoryService } from "./category.service";
 import { CategoryController } from "./category.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Category } from "./entities/category.entity";
 
 @Module({
-    exports:[CategoryModule,CategoryService],
+    imports: [TypeOrmModule.forFeature([Category])],
+    exports: [CategoryService],
     providers:[CategoryService],
     controllers:[CategoryController]
 })
